55ce8ff6e231f8aecaa16ef2d98430ad6da9273a
goziecodes <ezechukwuchigozie@gmail.com>
2020-03-28 00:46:32 +0100

    updated with shop page, collection preview component, collection item component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const Hatspage = () => (
-  <div>
-    <h1>I am hatspage </h1>
-  </div>
-)
 
 
 function App() {
   return (
     <div>
       {/* <HomePage /> */}
+      <switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/shop/hats' component={Hatspage} />
+      <Route  path='/shop' component={ShopPage} />
+      </switch>
     </div>
   );
 }
